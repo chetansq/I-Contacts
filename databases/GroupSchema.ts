841e64dd674c60595085e569970038ea28c177b2
chetan <chetan.sq05@gmail.com>
2025-02-04 18:11:27 +0530

    database in groupschema

---
+++ databases/GroupSchema.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+import { IGroup } from "../models/IGroup";
+
+const GroupSchema = new mongoose.Schema<IGroup>({
+
+    name: { type: String, required: true, unique: true },
+}, { timestamps: true });
+
+const GroupTable = mongoose.model<IGroup>("groups", GroupSchema);
+
+export default GroupTable;
---
+++ models/IGroup.ts
@@ -0,0 +1,6 @@
+export interface IGroup {
+    _id?: number;
+    name: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
